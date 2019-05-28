c057f9cf583793beb71cfbe0cf9c030bdc3bc59b
Patrick_B <pb5191@gmail.com>
2019-05-28 12:10:34 -0400

    added card texts

--- create-card.js
+++ create-card.js
@@ -3,26 +3,31 @@ const content = [
     title: "WHO I AM",
     pixUrl: "https://via.placeholder.com/200x200?text=Cachou+Image",
     text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta \n \n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta"
+      "My name is Cachou and I am a male Siberian Neva Masquerade. It does not look like it because I am a big cat, as my breed is known for, but I am still a kitten, just 9 months old. Right now, I weigh a bit more than 14 lbs! \n \n My main servant (cats do not have owners, they have servants. Owners are against the Cat Code of Conduct!) was kind enough to build this site for me. I can share what I like, my projects, some more information about me and my breed. This is fun!"
   },
   {
-    title: "My interests",
+    title: "MORE ABOUT SIBERIANS",
     pixUrl: "https://via.placeholder.com/200x200?text=Cachou+Image",
     text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta"
+      "We are a very old breed, sturdy and powerful. We reach our full adult size at 3 to 5 years old and we can weigh in at 20 to 25 lbs. Some say we are the base breed for all long-hair cats. Great jumpers, we are also very good hunters. Our personality is on the mellow side and we are truly chill cats. We love to play and be around our servants, just in case they get lost and need to be reminded of our presence. \n\n To know more, click on this Wikipedia link."
   },
   {
-    title: "Projects",
+    title: "NEVA MASQUERADE?",
     pixUrl: "https://via.placeholder.com/200x200?text=Cachou+Image",
     text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta"
+      "Though Siberians are usually tabbies or tortoise, my color is labeled as seal point. It means that at some point Siamese genes were introduced in our genetic make-up. My coloration is sometimes considered as a distinct Siberian breed."
   },
-
   {
-    title: "Other Happy cats",
+    title: "WHAT I LIKE",
     pixUrl: "https://via.placeholder.com/200x200?text=Cachou+Image",
     text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta"
+      "PLAYING - Like all cats, fluffy moving things are made for us to chase. What a blast! \n\n EATING & SLEEPING - I am really growing up and I need good meals. Kittens my age usually sleep 18 to 20 hours a day!\n\n BEING PET - There is almost nothing better than a nice rub behind the ears!"
+  },
+  {
+    title: "PROJECTS",
+    pixUrl: "https://via.placeholder.com/200x200?text=Cachou+Image",
+    text:
+      "BIRDS - There is a lot of nature around our place and those little fluttering zipping birds do look yummy on the other side of my window! I really, really want to hunt one of those blue jays I can see every day outside. \n\n EXPLORING - I am soooo curious, all those nooks and crannies in the house must be explored. I'd love to inspect all of them as well as jumping on all the ledges I can find."
   }
 ];
 
